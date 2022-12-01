0d098da08eb8515881ebeabbdb62173d4579cd58
pakoruales27 <panchitorualesf@hotmail.com>
2022-12-01 17:45:06 -0500

    commit 17:45

---
+++ app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity2.java
@@ -0,0 +1,14 @@
+package fisei.ti.francisco_ruales_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity3.java
@@ -0,0 +1,14 @@
+package fisei.ti.francisco_ruales_prueba01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
