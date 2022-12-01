43762531a68631600a8fef049a6f01675c4885ff
pakoruales27 <panchitorualesf@hotmail.com>
2022-12-01 18:15:43 -0500

    commit 18:15

--- app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity.java
+++ app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity.java
@@ -2,9 +2,19 @@ package fisei.ti.francisco_ruales_prueba01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void exectued(EditText editText){
+        editText.setText(null);
+        editText.setEnabled(false);
+        editText.setCursorVisible(false);
+        editText.setKeyListener(null);
+        editText.setBackgroundColor(Color.TRANSPARENT);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
