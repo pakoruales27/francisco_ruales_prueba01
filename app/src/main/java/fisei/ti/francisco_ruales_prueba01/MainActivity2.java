d1f36322595987ebe78662dbd074e5e545c9c09d
pakoruales27 <panchitorualesf@hotmail.com>
2022-12-01 18:45:44 -0500

    commit 18:45

--- app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity2.java
+++ app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity2.java
@@ -3,8 +3,12 @@ package fisei.ti.francisco_ruales_prueba01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    EditText EditTextNombre;
+    EditText EditTextApellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity3.java
+++ app/src/main/java/fisei/ti/francisco_ruales_prueba01/MainActivity3.java
@@ -3,12 +3,19 @@ package fisei.ti.francisco_ruales_prueba01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    EditText EditTextDividendo;
+    EditText EditTextDivisor;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        //EditTextDividendo = findViewById();
     }
 }
